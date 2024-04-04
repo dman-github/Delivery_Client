0199b6fb6e64c05d69c89142afde39ba44e63246
Damitha Arachchige <Damitha.Arachchige@sensium.co.uk>
2024-04-04 10:55:31 +0100

    added location permissions

--- app/src/main/java/com/okada/rider/android/MainActivity.kt
+++ app/src/main/java/com/okada/rider/android/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.okada.rider.android
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,11 +17,17 @@ import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.okada.rider.android.databinding.ActivityMainBinding
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.annotations.AfterPermissionGranted
+import com.vmadalin.easypermissions.dialogs.DEFAULT_SETTINGS_REQ_CODE
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: ActivityMainBinding
-
+    companion object {
+        private const val REQUEST_LOCATION_PERMISSION = 0x2233
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,5 +50,66 @@ class MainActivity : AppCompatActivity() {
 
         /* if we do not want the Action bar then we have to use code below */
         //setupWithNavController(navView,navController)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("App_Info", "onStart")
+        appRequiresPermission()
+    }
+
+    @AfterPermissionGranted(REQUEST_LOCATION_PERMISSION)
+    private fun appRequiresPermission() {
+        Log.i("App_Info", "appRequiresPermission called")
+        if (hasLocationPermission()) {
+            // Already have permission
+            Log.i("App_Info", "ALREADY permissions granted!")
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this,
+                getString(R.string.rationale_message_for_location),
+                REQUEST_LOCATION_PERMISSION,
+                ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.i("App_Info", "onRequestPermissionsResult permissions count: ${permissions.size}")
+        // EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == DEFAULT_SETTINGS_REQ_CODE) {
+            val yes = getString(R.string.yes)
+            val no = getString(R.string.no)
+            // Do something after user returned from app settings screen, like showing a Toast.
+            Toast.makeText(
+                this,
+                getString(R.string.returned_from_app_settings_to_activity, if (hasLocationPermission()) yes else no), LENGTH_LONG).show()
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        // Comes here whenever permission has been denied
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            // This will display a dialog directing them to enable the permission in app settings.
+            Log.i("App_Info", "onPermissionsDenied show settings screen")
+            SettingsDialog.Builder(this).build().show()
+        } else {
+            //Prompt again
+            appRequiresPermission()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        Log.i("App_Info", "onPermissionsGranted called")
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return EasyPermissions.hasPermissions(this, ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION)
     }
 }
