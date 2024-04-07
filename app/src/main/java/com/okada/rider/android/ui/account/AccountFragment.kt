d044b56081b88454754bfac16208e9eed5d7d7d2
Damitha Arachchige <Damitha.Arachchige@sensium.co.uk>
2024-04-07 22:38:45 +0100

    fragment account layout in progress

--- app/src/main/java/com/okada/rider/android/ui/account/AccountFragment.kt
+++ app/src/main/java/com/okada/rider/android/ui/account/AccountFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.okada.rider.android.databinding.FragmentNotificationsBinding
+import com.okada.rider.android.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentAccountBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,10 @@ class AccountFragment : Fragment() {
         val accountViewModel =
             ViewModelProvider(this).get(AccountViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        accountViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
