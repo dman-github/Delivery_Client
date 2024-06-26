4095674e29e89b1b90165d053c443f288a87127d
Damitha Arachchige <Damitha.Arachchige@sensium.co.uk>
2024-06-26 21:45:46 +0100

    checkpoint

---
+++ app/src/main/java/com/okada/rider/android/data/model/JobInfoModel.kt
@@ -0,0 +1,32 @@
+package com.okada.rider.android.data.model
+
+
+data class JobInfoModel (var jobId: String? = null,
+                       var driverUid: String? = null,
+                       var clientUid: String? = null,
+                       var status: String? = null,
+                       var jobDetails: JobDetails? = null
+) {
+    // Null default values create a no-argument default constructor, which is needed
+    // for deserialization from a DataSnapshot.
+}
+
+data class JobDetails(
+    var type: String? = null,
+    var info: String? = null,
+    var pickupLocation: Location? = null,
+    var deliveryLocation: Location? = null
+)
+
+
+data class Location(
+    var latitude: Double? = null,
+    var longitude: Double? = null
+)
+
+enum class JobStatus {
+    NEW,
+    ACCEPTED,
+    IN_PROGRESS,
+    COMPLETED
+}
---
+++ app/src/main/java/com/okada/rider/android/data/model/enums/JobStatus.kt
@@ -0,0 +1,9 @@
+package com.okada.rider.android.data.model.enums
+
+enum class JobStatus {
+    NEW,
+    ACCEPTED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
