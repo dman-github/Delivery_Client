9ba0355bfef80b4aef16aedb7160aaa139a7e69c
Damitha <damitha.arachchige@sensium.co.uk>
2024-08-30 10:31:56 +0100

    1.12 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.okada.rider.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.11"
+        versionCode = 13
+        versionName = "1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
